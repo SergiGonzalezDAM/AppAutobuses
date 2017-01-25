66c4d6a0e687286cf0dfc5626a7e7917f5936979
ALUMNEDAM <job_sergi@hotmail.es>
2017-01-25 19:02:22 +0100

    Servicio de segundo plano en pruebas 2

--- app/src/main/java/com/sergigonzalez/appautobuses/Servicio.java
+++ app/src/main/java/com/sergigonzalez/appautobuses/Servicio.java
@@ -2,27 +2,40 @@ package com.sergigonzalez.appautobuses;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.widget.Toast;
 
+/**
+ * Created by ALUMNEDAM on 20/12/2016.
+ */
 
-public class Servicio extends Service{
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-
-        return null;
-    }
+public class Servicio extends Service {
+    MediaPlayer reproductor;
 
     @Override
     public void onCreate() {
-        super.onCreate();
-        System.out.println("FUnciona");
+        Toast.makeText(this,"Servicio creado",
+                Toast.LENGTH_SHORT).show();
+        reproductor = MediaPlayer.create(this, R.raw.audio);
+    }
+
+    @Override
+    public int onStartCommand(Intent intenc, int flags, int idArranque) {
+        Toast.makeText(this,"Servicio arrancado "+ idArranque,
+                Toast.LENGTH_SHORT).show();
+        reproductor.start();
+        return START_STICKY;
     }
 
     @Override
     public void onDestroy() {
-        super.onDestroy();
-        System.out.println("Fuera");
+        Toast.makeText(this,"Servicio detenido",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public IBinder onBind(Intent intencion) {
+        return null;
     }
 }
